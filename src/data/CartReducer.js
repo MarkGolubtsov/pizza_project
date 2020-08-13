7a5b89a2657c7fbc9bdb44a7f966a3d8b915bb92
Elizabetter <isayl000@mail.ru>
2020-08-13 22:13:39 +0300

    cartreducer

--- src/data/CartReducer.js
+++ src/data/CartReducer.js
@@ -7,34 +7,35 @@ export const CartReducer = (storeData, action) => {
     switch (action.type) {
 
         case ActionTypes.CART_ADD_REMOVE:
-
-            const t = action.payload.topping;
-
-            const existing = newStore.cart.find(item => item.topping.id === t.id);
-            if (existing) {
-
-                newStore.cartCost -= existing.topping.cost;
-                newStore.cart = newStore.cart.filter(item => item !== existing);
-                return newStore;
-
-            } else {
-                newStore.cart = [...newStore.cart, action.payload];
-                newStore.cart = newStore.cart.map(item => {
-                    if (item.topping.id === t.id) {
-                        newStore.cartCost += item.topping.cost;
-                        return action.payload;
-                    } else {
-                        return item;
-                    }
-                });
-
-            }
-            return newStore;
+            return processCartAddRemove(newStore, action);
 
         case ActionTypes.CART_CLEAR:
             return {...storeData, cart: [], cartCost: 2};
 
         default:
             return storeData || {};
+    }
+};
+
+
+const processCartAddRemove = (state, action) => {
+    const t = action.payload.topping;
+    const existing = state.cart.find(item => item.topping.id === t.id);
+    if (existing) {
+        state.cartCost -= existing.topping.cost;
+        state.cart = state.cart.filter(item => item !== existing);
+        return state;
+
+    } else {
+        state.cart = [...state.cart, action.payload];
+        state.cart = state.cart.map(item => {
+            if (item.topping.id === t.id) {
+                state.cartCost += item.topping.cost;
+                return action.payload;
+            } else {
+                return item;
+            }
+        });
+        return state;
     }
 };
